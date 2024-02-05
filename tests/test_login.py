20399870d35b90cbfbff1b07cf735ea35267d537
Gilorkar <MGILORKA@jaguarlandrover.com>
2024-02-05 07:07:24 +0000

    added parametrization

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,41 +7,38 @@ from base.automation_wrapper import WebDriverWrapper
 
 
 class TestLogin(WebDriverWrapper):
-    def test_valid_login(self):
-        self.driver.find_element(By.ID, "authUser").send_keys("admin")
-        self.driver.find_element(By.ID, "clearPass").send_keys("pass")
-        self.driver.find_element(By.XPATH, "//button[@id='login-button']").click()
-        actual_title = self.driver.title
-        assert_that("OpenEMR Login").is_equal_to(actual_title)
-
-    def test_invalid_login(self):
-        self.driver.find_element(By.ID, "authUser").send_keys("john")
-        self.driver.find_element(By.CSS_SELECTOR, "#clearPass").send_keys("john123")
+    @pytest.mark.parametrize("username, password, expected_title",
+                             [
+                                 ("admin", "pass", "OpenEMR"),
+                                 ("accountant", "accountant", "OpenEMR")
+                             ]
+                             )
+    def test_valid_login(self, username, password, expected_title):
+        self.driver.find_element(By.ID, "authUser").send_keys(username)
+        self.driver.find_element(By.CSS_SELECTOR, "#clearPass").send_keys(password)
         self.driver.find_element(By.ID, "login-button").click()
-        actual_error=self.driver.find_element(By.XPATH,"//p[contains(text(),'Invalid')]").text
-        assert_that(actual_error).contains("Invalid username or password")
+        assert_that(expected_title).is_equal_to(self.driver.title)
 
+        def test_invalid_login(self):
+            self.driver.find_element(By.ID, "authUser").send_keys("john")
+            self.driver.find_element(By.CSS_SELECTOR, "#clearPass").send_keys("john123")
+            self.driver.find_element(By.ID, "login-button").click()
+            actual_error = self.driver.find_element(By.XPATH, "//p[contains(text(),'Invalid')]").text
+            assert_that(actual_error).contains("Invalid username or password")
 
+    class TestLoginUI(WebDriverWrapper):
+        def test_title(self):
+            print(self.driver)
+            actual_title = self.driver.title
+            assert_that("OpenEMR Login").is_equal_to(actual_title)
 
-class TestLoginUI(WebDriverWrapper):
+        def test_app_description(self):
+            actual_desc = self.driver.find_element(By.XPATH, "//p[contains(text(),'most')]").text
+            assert_that(actual_desc).contains("Electronic Health Record and Medical Practice Management")
 
-    def test_title(self):
-        print(self.a)
-        print(self.driver)
-        self.driver.implicitly_wait(5)
-        actual_title = self.driver.title
-        # assert_that("OpenEMR Login").is_equal_to(actual_title)
-        assert_that(actual_title).contains("OpenEMR Login")
-
-    def test_app_description(self):
-        actual_desc = self.driver.find_element(By.XPATH, "//p[contains(text(),'most')]").text
-        print(self.name)
-        assert_that(actual_desc).contains("Electronic Health Record and Medical Practice Management")
-
-    def test_placeholder(self):
-        actual_placeholder_username = self.driver.find_element(By.XPATH, "//input[@id='authUser']").get_attribute(
-            "placeholder")
-        actual_placeholder_password = self.driver.find_element(By.XPATH, "//input[@id='clearPass']").get_attribute(
-            "placeholder")
-        assert_that("Username").is_equal_to(actual_placeholder_username)
-        assert_that("Password").is_equal_to(actual_placeholder_password)
+        def test_placeholder(self):
+            actual_username_placeholder = self.driver.find_element(By.ID, "authUser").get_attribute("placeholder")
+            actual_password_placeholder = self.driver.find_element(By.CSS_SELECTOR, "#clearPass").get_attribute(
+                "placeholder")
+            assert_that("Username").is_equal_to(actual_username_placeholder)
+            assert_that("Password").is_equal_to(actual_password_placeholder)
