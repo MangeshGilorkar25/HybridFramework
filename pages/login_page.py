fdecf28fe9a8d40aac69b653dcaf6261a7c4ac3d
Gilorkar <MGILORKA@jaguarlandrover.com>
2024-02-05 08:48:43 +0000

    added login page object

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,2 +1,21 @@
+from selenium.webdriver.common.by import By
+from assertpy import assert_that
+
+
 class LoginPage:
-    pass
+
+    def __init__(self, driver):
+        self.__driver = driver
+
+    def enter_username(self, username):
+        self.__driver.find_element(By.ID, "authUser").send_keys(username)
+
+    def enter_password(self, password):
+        self.__driver.find_element(By.CSS_SELECTOR, "#clearPass").send_keys(password)
+
+    # click on login
+
+    def click_login(self):
+        self.__driver.find_element(By.ID, "login-button").click()
+
+    # get_error_message()
