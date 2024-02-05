d5776754a40da8b421cf95c92da57a58872bea9f
Gilorkar <MGILORKA@jaguarlandrover.com>
2024-02-05 05:16:43 +0000

    UI related test

--- tests/test_login.py
+++ tests/test_login.py
@@ -21,6 +21,7 @@ class TestLoginUI:
     def test_title(self):
         print(self.a)
         print(self.driver)
+        self.driver.implicitly_wait(5)
         actual_title = self.driver.title
         assert_that("OpenEMR Login").is_equal_to(actual_title)
 
@@ -29,3 +30,10 @@ class TestLoginUI:
         print(self.name)
         assert_that(actual_desc).contains("Electronic Health Record and Medical Practice Management")
 
+    def test_placeholder(self):
+        actual_placeholder_username = self.driver.find_element(By.XPATH, "//input[@id='authUser']").get_attribute(
+            "placeholder")
+        actual_placeholder_password = self.driver.find_element(By.XPATH, "//input[@id='clearPass']").get_attribute(
+            "placeholder")
+        assert_that("Username").is_equal_to(actual_placeholder_username)
+        assert_that("Password").is_equal_to(actual_placeholder_password)
