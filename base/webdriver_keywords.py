f19edbac4bd1d4f97e52d4feecef7f3d1c758dcd
Gilorkar <MGILORKA@jaguarlandrover.com>
2024-02-06 07:30:37 +0000

    new page added for frame and search patient

--- base/webdriver_keywords.py
+++ base/webdriver_keywords.py
@@ -3,13 +3,14 @@ from typing import Tuple
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
+"""This module contains all the webdriver keywords"""
 
 class WebDriverKeywords:
     def __init__(self, driver):
         self.__driver = driver
         self.__wait = WebDriverWait(driver, 30)
 
-    def type_on_element(self, locator: Tuple[str, str], text: str):
+    def type_on_element(self, locator: Tuple[str, str], text:str):
         self.__wait.until(
             expected_conditions.visibility_of_element_located(locator)).send_keys(text)
 
@@ -21,9 +22,9 @@ class WebDriverKeywords:
         return self.__wait.until(
             expected_conditions.visibility_of_element_located(locator)).text
 
-    def get_attribute_from_element(self, locator: Tuple[str, str], attribute_name: str):
+    def get_attribute_from_element(self, locator: Tuple[str, str],attribute_name: str):
         return self.__wait.until(
             expected_conditions.visibility_of_element_located(locator)).get_attribute(attribute_name)
 
-    # switch_tab_by_title()
-    # get_text_and_handle_alert()
+    #switch_tab_by_title()
+    #get_text_and_handle_alert()
