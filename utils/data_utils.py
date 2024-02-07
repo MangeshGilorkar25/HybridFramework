cce06cdb6f30e811269febb7ad32582f44cb6849
Gilorkar <MGILORKA@jaguarlandrover.com>
2024-02-07 05:14:05 +0000

    aded json for the browser controler and read utils methods

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,10 +1,12 @@
-class DataSource:
-    data_valid_login = [
-        ("admin", "pass", "OpenEMR"),
-        ("accountant", "accountant", "OpenEMR")
-    ]
+from utils import read_utils
 
-    data_invalid_login = [
-        ("savl", "savl1234", "Invalid username or password"),
-        ("kim", "kim1234", "Invalid username or password")
-    ]
+
+class DataSource:
+    data_valid_login = [("admin", "pass", "OpenEMR"),
+                        ("accountant", "accountant", "OpenEMR")]
+    data_invalid_login = [("saul", "saul123", "Invalid username or password"),
+                          ("kim", "kim123", "Invalid username or password")]
+
+    data_valid_login_excel = read_utils.get_sheet_into_list("../test_data/open_emr_data.xlsx", "test_valid_login")
+    data_invalid_login_excel = read_utils.get_sheet_into_list("../test_data/open_emr_data.xlsx", "test_invalid_login")
+    data_valid_login_csv = read_utils.get_csv_into_list("../test_data/test_valid_login.csv")
--- utils/demo1_pandas.py
+++ utils/demo1_pandas.py
@@ -1,1 +1,25 @@
-# will be deleted not part of the framework
+"""Will be deleted later - not part of the framework"""
+
+import pandas
+
+df = pandas.read_excel(io="../test_data/open_emr_data.xlsx", sheet_name="test_valid_login")
+print(df)
+# print(df.loc[1])
+# print(df.columns)
+# print(df.index)
+print(df.values.tolist())
+
+# alernate way
+ls = []
+for i in df.index:
+    print(df.loc[i].tolist())
+    ls.append(df.loc[i].tolist())
+
+print(ls)
+
+df = pandas.read_csv(filepath_or_buffer="../test_data/test_valid_login.csv", delimiter=";")
+print(df)
+print(df.values.tolist())
+
+json_dic = pandas.read_json(path_or_buf="../test_data/config.json", typ="dictionary")
+print(json_dic)
